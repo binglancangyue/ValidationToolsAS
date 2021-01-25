4c30d1b8019d0d2d7f67542017b29498f9300f64
Altair <binglancangyue@163.com>
2021-01-25 16:00:39 +0800

    t10适配

--- app/src/main/java/com/sprd/validationtools/itemstest/ScreenColorTest.java
+++ app/src/main/java/com/sprd/validationtools/itemstest/ScreenColorTest.java
@@ -1,20 +1,15 @@
 
 package com.sprd.validationtools.itemstest;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sprd.validationtools.BaseActivity;
-import com.sprd.validationtools.R;
 import com.sprd.validationtools.Const;
+import com.sprd.validationtools.R;
 
 public class ScreenColorTest extends BaseActivity {
     private String TAG = "ScreenColorTest";
